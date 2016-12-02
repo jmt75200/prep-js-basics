285d62692b6ba3846f8891497520ead41516d05c
Joelle Torneros <jm.torneros@gmail.com>
2016-12-02 09:15:22 -1000

    solutions for test

---
+++ js_test_review.js
@@ -0,0 +1,54 @@
+/*
+  Joelle Torneros
+*/
+
+// Variables
+
+var knight = "Arthur";
+var maiden = "Jane";
+var monster = "TanDragon";
+
+// Array
+var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"];
+
+// Object
+var hero = {
+  wizard : "Merlin",
+  weapon : weaponChest[2],
+  power : 9001
+}
+
+// Sword in the stone
+function swordInTheStone(worthy) {
+  if (worthy == "Arthur") {
+    console.log("We have found our King " + worthy);
+  } else {
+    console.log("We must keep searching, for " + worthy + " is unworthy");
+  }
+}
+
+swordInTheStone(knight);
+
+// True Love
+function trueLove(king, queen) {
+  if (queen == "Guiverene" && king == "Arthur" ) {
+    console.log("On this day, our " + king + " "+"and"+" " +queen + " shall rule happily ever after.");
+  } else {
+    console.log("We must keep searching, for true love must reign over Camelot.");
+  }
+}
+
+trueLove(knight, maiden);
+
+// Epic Battle
+function epicBattle(wizard, enemy, powa) {
+  if (powa <= 15) {
+    console.log("Our " + wizard + " has fallen.");
+  } else if (powa >= 16 && powa <= 40) {
+    console.log("Our " + wizard + " has almost slain the " + enemy);
+  } else {
+    console.log("Our " + wizard + " has slain the " + enemy);
+  }
+}
+
+epicBattle(hero.wizard, monster, hero.power);
